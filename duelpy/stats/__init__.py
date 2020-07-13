4a30b397b5a8b04000aa1ee730347c84a55ed022
Timo Kaufmann <timokau@zoho.com>
2020-07-13 15:39:23 +0200

    fixup! Rename util to stats
    As (indirectly) suggested by Alexander on Zulip.

--- duelpy/stats/__init__.py
+++ duelpy/stats/__init__.py
@@ -1,5 +1,5 @@
 """Utilities for implementation of PB-MABA algorithms."""
 
-from duelpy.util.preference_estimate import PreferenceEstimate
+from duelpy.stats.preference_estimate import PreferenceEstimate
 
 __all__ = ["PreferenceEstimate"]
