013ba77c7cdc435f5fb8b348ad13834513dc869b
Timo Kaufmann <timokau@zoho.com>
2020-08-28 18:16:56 +0200

    Add a function to determine Copeland winners

--- duelpy/feedback/preference_matrix.py
+++ duelpy/feedback/preference_matrix.py
@@ -3,11 +3,13 @@
 from typing import Callable
 from typing import List
 from typing import Optional
+from typing import Set
 from typing import Tuple
 
 import numpy as np
 
 from duelpy.feedback.feedback_mechanism import FeedbackMechanism
+from duelpy.util.utility_functions import argmax_set
 
 
 class PreferenceMatrix(FeedbackMechanism):
@@ -85,6 +87,28 @@ class PreferenceMatrix(FeedbackMechanism):
             if np.amin(preference_probabilities) > 0.5:
                 return arm_idx
         return None
+
+    def get_copeland_winners(self) -> Set[int]:
+        """Get the set of Copeland winners.
+
+        A Copeland winner is an arm that has the highest number of expected
+        wins against all other arms. This does not need to be unique, since
+        multiple arms can have the same number of expected wins.
+
+        Returns
+        -------
+        Set[int]
+            The indices of the Copeland winners.
+        """
+        num_arms = len(self.arms)
+        expected_wins = np.zeros(num_arms)
+        for first_arm_idx in range(num_arms):
+            for second_arm_idx in range(first_arm_idx + 1, num_arms):
+                if self.preference_matrix[first_arm_idx, second_arm_idx] > 1 / 2:
+                    expected_wins[first_arm_idx] += 1
+                else:
+                    expected_wins[second_arm_idx] += 1
+        return argmax_set(expected_wins)
 
     def reset_history(self) -> None:
         """Delete the regret history."""
