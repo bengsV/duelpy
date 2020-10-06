ac87f7a067ad748931cc8cdbc41b92d61869e67f
Timo Kaufmann <timokau@zoho.com>
2020-10-06 11:34:16 +0200

    Optimize argmin_set and argmax_set

--- duelpy/util/utility_functions.py
+++ duelpy/util/utility_functions.py
@@ -26,9 +26,12 @@ def argmin_set(array: np.array, exclude_indexes: Optional[Set[int]] = None) -> n
     # np.argmin only returns the first index, to get the whole set,
     # we first find the minimum and then search for all indices which point
     # to a value equal to this minimum
+    if exclude_indexes is None:
+        # For this case the simpler implementation is more efficient, although
+        # the other one with a trivial mask would also work.
+        return np.argwhere(array == np.amin(array)).flatten()
     mask = np.zeros(array.size, dtype=bool)
-    if exclude_indexes is not None:
-        mask[exclude_indexes] = True
+    mask[exclude_indexes] = True
     min_value = np.min(np.ma.array(array, mask=mask))
     indices = set(np.ndarray.flatten(np.argwhere(array == min_value)))
     indices = indices - set(exclude_indexes) if exclude_indexes is not None else indices
@@ -56,9 +59,12 @@ def argmax_set(array: np.array, exclude_indexes: Optional[Set[int]] = None) -> n
     # np.argmax only returns the first index, to get the whole set,
     # we first find the maximum and then search for all indices which point
     # to a value equal to this maximum
+    if exclude_indexes is None:
+        # For this case the simpler implementation is more efficient, although
+        # the other one with a trivial mask would also work.
+        return np.argwhere(array == np.amax(array)).flatten()
     mask = np.zeros(array.size, dtype=bool)
-    if exclude_indexes is not None:
-        mask[exclude_indexes] = True
+    mask[exclude_indexes] = True
     max_value = np.max(np.ma.array(array, mask=mask))
     indices = set(np.ndarray.flatten(np.argwhere(array == max_value)))
     indices = indices - set(exclude_indexes) if exclude_indexes is not None else indices
