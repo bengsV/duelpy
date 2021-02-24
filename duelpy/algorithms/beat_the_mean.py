78839ae83a3335b26d2811ce1386f58c6a0205a4
Alexander Werning <awerning@mail.upb.de>
2021-02-24 11:59:20 +0100

    Add glossary entries BTM

--- duelpy/algorithms/beat_the_mean.py
+++ duelpy/algorithms/beat_the_mean.py
@@ -15,16 +15,16 @@ from duelpy.util.utility_functions import argmin_set
 
 
 class BeatTheMeanBandit(CondorcetProducer, PacAlgorithm):
-    r"""Implements the 'Beat the Mean Bandit' algorithm.
+    r"""Implements the Beat the Mean Bandit algorithm.
 
-    The goal of this algorithm is to find the Condorcet winner.
+    The goal of this algorithm is to find the :term:`Condorcet winner`.
 
-    It is assumed that a total order over the arms exists. Additionally relaxed stochastic transitivity and the
-    stochastic triangle inequality are assumed.
+    It is assumed that a :term:`total order` over the arms exists. Additionally :term:`relaxed stochastic transitivity` and the
+    :term:`stochastic triangle inequality` are assumed.
 
-    The online version (if a time horizon is supplied) has a high probability cumulative regret bound of :math:`O(\frac{\gamma^7 N}{\epsilon_\ast} \log T)`.
-    The :math:`\gamma` is part of the :math:`\gamma`-relaxed transitivity assumption. :math:`N` is the number of
-    arms. :math:`\epsilon_\ast` is the winning probability of the best arm against the second best arm minus 0.5.
+    The online version (if a time horizon is supplied) has a high probability cumulative regret bound of :math:`\mathcal{O}\left(\frac{\gamma^7 N}{\epsilon_\ast} \log T\right)`.
+    The :math:`\gamma` is part of the :math:`\gamma`-:term:`relaxed stochastic transitivity` assumption. :math:`N` is the number of
+    arms. :math:`\epsilon_\ast` is the winning probability of the best arm against the second best arm minus :math:`0.5`.
 
     This is an explore-then-exploit algorithm. The Beat the Mean algorithm, as described in :cite:`yue2011beat`,
     proceeds in a sequence of rounds and maintains a working set of active arms during each round. For each active
@@ -43,20 +43,18 @@ class BeatTheMeanBandit(CondorcetProducer, PacAlgorithm):
     random_state
         A numpy random state. Defaults to an unseeded state when not specified.
     gamma
-        The relaxed stochastic transitivity (corresponds to :math:`\gamma` in :cite:`yue2011beat`) that the
+        The :term:`relaxed stochastic transitivity` (corresponds to :math:`\gamma` in :cite:`yue2011beat`) that the
         algorithm should assume for the given problem setting. The value must be greater than :math:`0`.
-        A higher value corresponds to a stronger assumption, where :math:`1` corresponds to strong stochastic
-        transitivity. In theory it is not possible to assume more than a gamma of :math:`1`, but in practice you can
+        A higher value corresponds to a stronger assumption, where :math:`1` corresponds to :term:`strong stochastic
+        transitivity`. In theory it is not possible to assume more than a gamma of :math:`1`, but in practice you can
         still specify higher values. This will lead to tighter confidence intervals and possibly better results,
-        but the theoretical guarantees do not hold in that case. Formally this corresponds to the following
-        assumed property of the calibrated preference matrix: :math:`\Delta_{i, j} \ge \gamma \max \{ \Delta_{i, j}, \Delta{j, k} \}` should hold for all pairwise distinct indices with :math:`\Delta_{i, j} \ge 0` and
-        :math:`\Delta_{j, k} \ge 0`.
+        but the theoretical guarantees do not hold in that case.
 
 
     Attributes
     ----------
     comparison_history
-        A ComparisonHistory object which stores the history of the comparisons between the arms.
+        A ``ComparisonHistory`` object which stores the history of the comparisons between the arms.
     random_state
     feedback_mechanism
     time_horizon
@@ -182,7 +180,7 @@ class ComparisonHistory:
     working_set
         Stores the set of active arms. Corresponds to :math:`W_l` in :cite:`yue2011beat`.
     size_working_set
-        Stores the amount of active arms in `working_set`. Using this instead of summing over `working_set` improves performance.
+        Stores the amount of active arms in ``working_set``. Using this instead of summing over ``working_set`` improves performance.
     comparisons
         Stores the total number of comparisons of a specific arm. Corresponds to :math:`n_b` in :cite:`yue2011beat`.
     wins
@@ -327,19 +325,19 @@ class ComparisonHistory:
 class BeatTheMeanBanditPAC(BeatTheMeanBandit):
     r"""The PAC variant of the Beat the Mean Bandit algorithm.
 
-    The goal of this algorithm is to find the Condorcet winner.
+    The goal of this algorithm is to find the :term:`Condorcet winner`.
 
-    It is assumed that a total order over the arms exists. Additionally relaxed stochastic transitivity and the stochastic triangle inequality are assumed.
+    It is assumed that a :term:`total order` over the arms exists. Additionally :math:`\gamma`-:term:`RST<Relaxed Stochastic Transitivity>` and the :term:`STI<Stochastic Triangle Inequality>` are assumed.
 
-    In the PAC setting (no time horizon given), the sample complexity is bound by :math:`O\left(\frac{N \gamma^6}{\epsilon^2} \log\frac{Nc}{\delta}\right)`.
+    In the :term:`PAC` setting (no time horizon given), the sample complexity is bound by :math:`O\left(\frac{N \gamma^6}{\epsilon^2} \log\frac{Nc}{\delta}\right)`.
     The constant :math:`c` is given as :math:`\left\lceil \frac{36}{\gamma^6\epsilon^2}\log \frac{N}{\delta}\right\rceil`.
 
-    The PAC setting for Beat the Mean Bandit algorithm takes an 'explore then exploit' approach. In PAC setting,
+    The :term:`PAC` setting for Beat the Mean Bandit algorithm takes an 'explore then exploit' approach. In :term:`PAC` setting,
     the exploration conditions for Beat the Mean Bandit algorithm is different from the Online setting. There are two
-    termination cases for PAC exploration, the first case is when the active set has been reduced to a single bandit.
+    termination cases for :term:`PAC` exploration, the first case is when the active set has been reduced to a single bandit.
     The second case is when the number of comparisons recorded for each remaining bandit is at least ``opt_n``
-    (Corresponds to :math:`N'` in section 3.1.2 in :cite:`busa2018preference`).We do not use
-    the time horizon in Beat-the-Mean PAC setting (i.e., we set ``time_horizon = None``); it is used only in the online setting.
+    (Corresponds to :math:`N'` in section 3.1.2 in :cite:`busa2018preference`). We do not use
+    the time horizon in Beat-the-Mean :term:`PAC` setting (i.e., we set ``time_horizon = None``); it is used only in the online setting.
 
     Parameters
     ----------
@@ -348,21 +346,19 @@ class BeatTheMeanBanditPAC(BeatTheMeanBandit):
     random_state
         A numpy random state. Defaults to an unseeded state when not specified.
     gamma
-        The relaxed stochastic transitivity (corresponds to :math:`\gamma` in :cite:`yue2011beat`) that the
+        The :math:`\gamma`-:term:`RST<Relaxed Stochastic Transitivity>` (corresponds to :math:`\gamma` in :cite:`yue2011beat`) that the
         algorithm should assume for the given problem setting. The value must be greater than :math:`0`.
-        A higher value corresponds to a stronger assumption, where :math:`1` corresponds to strong stochastic
-        transitivity. In theory it is not possible to assume more than a gamma of :math:`1`, but in practice you can
+        A higher value corresponds to a stronger assumption, where :math:`1` corresponds to :term:`STI<stochastic triangle inequality>`. In theory it is not possible to assume more than a gamma of :math:`1`, but in practice you can
         still specify higher values. This will lead to tighter confidence intervals and possibly better results,
-        but the theoretical guarantees do not hold in that case. Formally this corresponds to the following
-        assumed property of the calibrated preference matrix: :math:`\Delta_{i, j} \ge \gamma \max \{ \Delta_{i,j}, \Delta{j, k} \}` should hold for all pairwise distinct indices with :math:`\Delta_{i, j} \ge 0` and
-        :math:`\Delta_{j, k} \ge 0`. This parameter has been taken from the parent class.
+        but the theoretical guarantees do not hold in that case. This parameter has been taken from the parent class.
     epsilon
-        :math:`\epsilon` in (:math:`\epsilon,\delta`)PAC algorithms, given by the user.
+        :math:`\epsilon` in (:math:`\epsilon, \delta`) :term:`PAC` algorithms, given by the user.
     failure_probability
-        Allowed failure-probability (corresponds to :math:`\delta` in Algorithm 2 in :cite:`yue2011beat`),
+        Allowed failure probability (corresponds to :math:`\delta` in Algorithm 2 in :cite:`yue2011beat`),
         i.e. probability that the actual value lies outside of the computed confidence interval. Derived from the
         Hoeffding bound.
     time_horizon
+        Determines how many duels are executed in the online setting.
 
     Attributes
     ----------
