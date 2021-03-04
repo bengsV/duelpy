0eaf5b318f0f9b8768eaaac5e80aa9a2e55b0a57
Timo Kaufmann <timokau@zoho.com>
2021-03-04 21:57:42 +0100

    Add remaining dependencies to install_requires

--- setup.py
+++ setup.py
@@ -15,7 +15,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/duelpy/duelpy",
     packages=setuptools.find_packages(),
-    install_requires=["numpy"],
+    install_requires=[
+        "numpy",
+        "seaborn",
+        "pandas",
+        "joblib",
+        "scipy",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
