367c566c0f0a56356d8e514189f525b0e1cfb003
Timo Kaufmann <timokau@zoho.com>
2021-03-24 11:12:16 +0100

    Use a decorator in the decision experiment
    Using `step` to print the preference estimate after each duel only works
    if `step` actually only performs one duel. That is the case for Savage,
    but not for many other algorithm implementations. The example is quite
    a bit longer now, but most of it is documentation. It gives a better
    insight into the library but the downside is that it might be a bit
    daunting as a first introduction.

--- duelpy/examples/decision.py
+++ duelpy/examples/decision.py
@@ -1,7 +1,59 @@
 """A simple example of how a decision-tool using duelpy could work."""
 
+from typing import Optional
+
 from duelpy.algorithms.savage import Savage
 from duelpy.feedback import CommandlineFeedback
+from duelpy.feedback import FeedbackMechanism
+from duelpy.stats import PreferenceEstimate
+from duelpy.util.feedback_decorators import FeedbackMechanismDecorator
+
+
+class PreferencePrintingFeedback(FeedbackMechanismDecorator):
+    """Prints a preference estimate before each duel.
+
+    The ``preference_estimate`` attribute needs to be set after initialization.
+    It is not passed in the constructor since it usually can only be accessed
+    after the algorithm has been initialized.
+
+    Parameters
+    ----------
+    feedback_mechanism
+        The ``FeedbackMechanism`` object to delegate to.
+
+    Attributes
+    ----------
+    feedback_mechanism
+        The ``FeedbackMechanism`` object to delegate to.
+    preference_estimate
+        The preference estimate that should be printed.
+    """
+
+    def __init__(self, feedback_mechanism: FeedbackMechanism) -> None:
+        super().__init__(feedback_mechanism)
+        self.preference_estimate: Optional[PreferenceEstimate] = None
+
+    def duel(self, arm_i_index: int, arm_j_index: int) -> bool:
+        """Print the preference estimate and then duel two arms.
+
+        The actual duel is delegated to the underlying preference estimate.
+
+        Parameters
+        ----------
+        arm_i_index
+            The index of challenger arm.
+        arm_j_index
+            The index of arm to compare against.
+
+        Returns
+        -------
+        bool
+            True if the first arm wins.
+        """
+        if self.preference_estimate is not None:
+            print("Preference estimate is now")
+            print(self.preference_estimate)
+        return super().duel(arm_i_index, arm_j_index)
 
 
 def _run_decision_experiment() -> None:
@@ -12,12 +64,13 @@ def _run_decision_experiment() -> None:
         "Third best arm.",  # 3
         "Least favorite arm.",  # 4
     ]
-    feedback_mechanism = CommandlineFeedback(arms)
-    algorithm = Savage(feedback_mechanism=feedback_mechanism, failure_probability=0.5)
-    while not algorithm.is_finished():
-        algorithm.step()
-        print("Preference estimate is now")
-        print(algorithm.preference_estimate)
+    pp_feedback = PreferencePrintingFeedback(CommandlineFeedback(arms))
+    algorithm = Savage(feedback_mechanism=pp_feedback, failure_probability=0.5)
+    # The preference estimate is initialized by the algorithm. Many algorithms
+    # have this attribute, but it is not part of the `Algorithm` API. We rely
+    # on the implementation of Savage here.
+    pp_feedback.preference_estimate = algorithm.preference_estimate
+    algorithm.run()
     print("Estimated Copeland winner:")
     print(algorithm.get_copeland_winner())
 
